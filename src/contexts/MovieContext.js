afe5a316c7b21909e09ed7b97160f7fb2139f1d9
tootboi <danielyim12@gmail.com>
2020-06-01 15:58:32 +1200

    app now uses local storage for persistent data entry

--- src/contexts/MovieContext.js
+++ src/contexts/MovieContext.js
@@ -1,13 +1,21 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useReducer, useEffect } from 'react';
 import { movieReducer } from '../reducers/movieReducer';
 
 export const MovieContext = createContext();
 
 const MovieContextProvider = (props) => {
     const [movies, dispatch] = useReducer(movieReducer, [
-        {title: 'Pulp Fiction', director: 'Quentin Tarantino', id: 'unique-id1'},
-        {title: 'The Grand Budapest Hotel', director: 'Wes Anderson', id: 'unique-id2'}
-    ]);
+        
+    ], () => {
+        const localData = localStorage.getItem('movies');
+        return localData ? JSON.parse(localData) : [
+            {title: 'Pulp Fiction', director: 'Quentin Tarantino', id: 'unique-id1'},
+            {title: 'The Grand Budapest Hotel', director: 'Wes Anderson', id: 'unique-id2'}
+        ];
+    });
+    useEffect(() => {
+        localStorage.setItem('movies', JSON.stringify(movies));
+    }, [movies]);
     return (
         <MovieContext.Provider value={{movies, dispatch}}>
             {props.children}
