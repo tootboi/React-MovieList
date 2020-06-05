4a3b04a153b0be6cf57073200a83db7b830dd619
tootboi <danielyim12@gmail.com>
2020-06-05 13:58:20 +1200

    Updates

---
+++ precache-manifest.912bc342f7b55c14939aa948ca9bc08b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e68fa687530aced26e10b5ad3125bf",
+    "url": "/React-MovieList/index.html"
+  },
+  {
+    "revision": "4100d98bac4343ccdb87",
+    "url": "/React-MovieList/static/css/main.1f9f2c8e.chunk.css"
+  },
+  {
+    "revision": "0a60d2409665f1e2e779",
+    "url": "/React-MovieList/static/js/2.27b6cb42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-MovieList/static/js/2.27b6cb42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4100d98bac4343ccdb87",
+    "url": "/React-MovieList/static/js/main.b954731f.chunk.js"
+  },
+  {
+    "revision": "37ebaa6111133d5e792a",
+    "url": "/React-MovieList/static/js/runtime-main.2d777309.js"
+  }
+]);
